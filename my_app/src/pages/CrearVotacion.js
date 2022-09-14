ae92060a274bec21fccf3f2bd6c109af57ba6b7a
matias <matias.jaque@usach.cl>
2022-09-14 12:41:34 -0300

    avances en la vista crear votacion

--- my_app/src/pages/CrearVotacion.js
+++ my_app/src/pages/CrearVotacion.js
@@ -9,10 +9,15 @@ import MyNavbar from '../componts/MyNavbar';
 
 import '../styles/CrearVotacion.css'
 
+import { AiFillCloseCircle } from "react-icons/ai";
+
+
+
 
 
 
 const CrearVotaciones = (props) => {
+  
 
   const [tituloVotacion, setTituloVotacion] = useState('');
   const [preguntaVotacion, setPreguntaVotacion] = useState([]);
@@ -24,54 +29,194 @@ const CrearVotaciones = (props) => {
 
   const [respuestas, setRespuestas] = useState([]);
   const [preguntas, setPreguntas] = useState([]);
+  var preguntasDemo = [];
+  var respuestasDemo = [];
+  var idPregunta = 0;
+  var idRespuesta = 0;
+
+  const [idResp, setIdResp] = useState(0)
+
+  var prueba = ['a', 'b'];
 
   const [kk, setKk] = useState(['uno', 'dos', 'tres']);
 
   const [datosVotacion, setDatosVotacion] = useState(false);
-  console.log(preguntas)
+
 
   const volverHome = () =>{
     window.location.replace('/paginaPrincipal')
   }
 
   const continuarVotacion = () =>{
-    setPreguntas()
     setDatosVotacion(false);
-    if(tituloVotacion !== null && cantidadPreg > 0  && cantidadResp > 0){
-      console.log('cantidadPreg ' + cantidadPreg); 
-      console.log('preguntas length ' + preguntas.length);
-      //var cantPreg = preguntas.length;
+    //setPreguntas()
+    // validos todos los parametros y que preguntas no tenga datos
+    if(tituloVotacion !== null && cantidadPreg > 0  && cantidadResp > 0 && preguntasDemo.length === 0 && respuestasDemo.length === 0) {
+      //console.log('cantidadPreg ' + cantidadPreg); 
+      //console.log('preguntasDemo length ' + preguntasDemo.length);
+      //var cantPreg = preguntasDemo.length;
       
       for(let i = 0; i < cantidadPreg; i++){
-        //preguntas.push(i+1);
-        console.log('hola')
-        setPreguntas([...preguntas, (i+1)])
-        console.log('chao')
+        //preguntasDemo.push(i+1);
+        //console.log('hola')
+          preguntasDemo.push(
+          {
+            id: idPregunta,
+            tituloPregunta: '',
+            respuestas : [],
+          })
+          for (let j = 0; j < cantidadResp; j++){
+            respuestasDemo.push({
+              idPregunta: idPregunta,
+              idRespuesta: idRespuesta,
+              tituloRespuesta: '',
+            })
+            idRespuesta ++;
+          }
+        //console.log('id de pregunta: ' + idPregunta);
+        
+        idPregunta ++;
       }
-      setDatosVotacion(true);
-      /* if(preguntas.length === 0){
-        for(let i = 0; i < cantidadPreg; i++){
-          preguntas.push(i+1);
-        }
-        setDatosVotacion(true);
-      } */
-      /* else{
-        for(let i = 0; i < cantPreg; i++){
-          preguntas.pop();
-          console.log('preguntas length del for ' + preguntas.length);
+      setIdResp(idRespuesta)
 
+      for (let i = 0; i <cantidadPreg; i++){
+        
+        for (let j = 0; j <respuestasDemo.length; j++){
+
+          //console.log(preguntasDemo[i].id + ' === ' + respuestasDemo[j].idPregunta)
+          if (preguntasDemo[i].id === respuestasDemo[j].idPregunta){
+            preguntasDemo[i].respuestas.push({
+              idRespuesta: respuestasDemo[j].idRespuesta,
+              tituloRespuesta: respuestasDemo[j].tituloRespuesta,
+              idPregunta: respuestasDemo[j].idPregunta,
+            })
+          }
         }
-        for(let i = 0; i < cantidadPreg; i++){
-          preguntas.push(i+1);
-          console.log('for del else ' + preguntas.length);
-        }
-      } */
+      }
+      /* console.log('preguntasDemo ');
+      console.log(preguntasDemo);
+      console.log('respuestasDemo ');
+      console.log(respuestasDemo); */
+
+      setDatosVotacion(true);
+      setPreguntas(preguntasDemo);
+      setRespuestas(respuestasDemo);/* 
+      console.log(datosVotacion);
+      console.log('preguntas de abajo ');
+      console.log(preguntasDemo);
+      console.log('largo preguntasDemo ' + preguntasDemo.length);
+      prueba.push('2') */
       
+  
+      //console.log('largo de prueba 1 ' + prueba.length);
     }
+
     else{
         alert("todos los parametros son obligatorios");
     }
   }
+
+  const eliminarPregunta = (idPreg) => {
+    setDatosVotacion(false);
+    console.log("id preg: " );
+    console.log(idPreg)
+
+    console.log('cantidad de preg antes: ')
+    console.log(cantidadPreg);
+
+    const newPreguntas = preguntas.filter((preg) => preg.id !== idPreg);
+    setPreguntas(newPreguntas);
+
+    setCantidadPreg(cantidadPreg - 1);
+
+    document.getElementById('cantPreg').value = cantidadPreg;
+
+    console.log('cantidad de preg desp: ')
+    console.log(cantidadPreg);
+
+    setDatosVotacion(true);
+
+  }
+
+  const eliminarRespuesta = (idResp, resp, idPreg) => {
+    setDatosVotacion(false);
+
+    console.log("id Resp: " );
+    console.log(idResp)
+    console.log("resp: " );
+    console.log(resp)
+
+    console.log("Respuestas : " );
+    console.log(respuestas)
+
+
+    const newPreguntas = preguntas.map((pregunta) => {
+    const newRespuestas = respuestas.filter((respu) => (respu.idRespuesta !== idResp && respu.idPregunta === pregunta.id ));
+    console.log(newRespuestas);
+       if (pregunta.id === idPreg) {
+
+        return {
+          ...pregunta,
+          respuestas: newRespuestas,
+        }
+       }
+       return pregunta
+
+    })
+
+    
+    const newRespuestas2 = respuestas.filter((respu) => respu.idRespuesta !== idResp);
+
+    setRespuestas(newRespuestas2);
+    setPreguntas(newPreguntas);
+    console.log(preguntas)
+
+    setDatosVotacion(true);
+  }
+
+  const agregarRespuesta = (idPregunta) => {
+    setDatosVotacion(false);
+    console.log(respuestas)
+    console.log(idResp)
+    console.log(idPregunta)
+
+    const newResp = {
+      idPregunta: idPregunta,
+      idRespuesta: idResp,
+      tituloRespuesta: '',
+    }
+
+    setRespuestas([...respuestas, newResp])
+
+    console.log('resp')
+    console.log(respuestas)
+      
+    setIdResp(idResp + 1)
+
+      const newRespuestas = respuestas.filter((respu) => (respu.idPregunta === idPregunta));
+      console.log('newRespuestas');
+
+      console.log(newRespuestas);
+
+      const newPreguntas = preguntas.map((preg) => {
+      if(preg.id === idPregunta){
+        return {
+          ...preg,
+          respuestas: newRespuestas,
+        }
+      }
+      return preg
+    })
+
+    setPreguntas(newPreguntas)
+    setDatosVotacion(true);
+
+  }
+
+  console.log('preg')
+  console.log(preguntas)
+  console.log('resp')
+  console.log(respuestas)
 
   return (
     <div id='contenedorPrincipalMisVotaciones'>
@@ -86,7 +231,6 @@ const CrearVotaciones = (props) => {
               </Col>
             </Row>
             
-            
             <Row className='filas'>
               <Col className='columnas'>
                 <Form.Label className="titulosForm">TÍTULO DE LA VOTACIÓN</Form.Label>
@@ -100,8 +244,10 @@ const CrearVotaciones = (props) => {
               <Col className='columnas'>
                 <Form.Label className="titulosForm">CANTIDAD DE PREGUNTAS DE LA VOTACIÓN</Form.Label>
                   <Form.Control className="textosForm"
+                      id="cantPreg"
                       type="text"
                       placeholder="Ingrese la cantidad de preguntas que tendra la votación"
+                      value={cantidadPreg}
                       onChange={(e) => setCantidadPreg(e.target.value)}
                   />
               </Col>
@@ -111,6 +257,7 @@ const CrearVotaciones = (props) => {
                   <Form.Control className="textosForm"
                       type="text"
                       placeholder="Ingrese la cantidad de respuestas por pregunta que tendra la votación"
+                      
                       onChange={(e) => setCantidadResp(e.target.value)}
                   />  
               </Col>
@@ -122,36 +269,52 @@ const CrearVotaciones = (props) => {
             </div>
                 
           </div>
-
+          
           {
-            datosVotacion? 
+            datosVotacion === true  ? 
             <div id='contenedorVotacion'>
-                {preguntas.map(() => (
+              {/* hola
+              {preguntas.map(e => <h1>{e.id}</h1>)} */}
+              
+                {preguntas.map((element) => (
                   <div id='contenedorForm'>
-                  <Form>
-                      <Form.Control type="text" placeholder="Ingresa el titulo de la votación" onChange={(e) =>{
-                        setTituloVotacion(e.target.value);
-                      }}/>
+                  <Form id='form'>
+                    <Row className='filas'>
+                      <Col sm md lg ={11} className='columnas'>
+                        <Form.Control id='inputPreg' type="text" placeholder="Ingresa el titulo de la pregunta" onChange={(e) =>{
+                          let nuevaPregunta = e.target.value;
+                          setPreguntaVotacion([...preguntaVotacion, nuevaPregunta]);
+                        }}/> 
+                      </Col>
 
-
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                      <Col>
+                        <AiFillCloseCircle id='iconoCerrar' onClick={() => eliminarPregunta(element.id)}/>
+                      </Col>
                       
-                      <Form.Control type="text" placeholder="Ingresa el titulo de la pregunta" onChange={(e) =>{
-                        let nuevaPregunta = e.target.value;
-                        setPreguntaVotacion([...preguntaVotacion, nuevaPregunta]);
-                      }}/>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                      
-                      <Form.Control type="text" placeholder="Ingresa la cantidad de respuestas que desea para esta pregunta" onChange={(e) =>{
-                        let cantidadRespuestas = e.target.value;
-                        setCantidadDeRespuestas([...cantidadDeRespuestas, cantidadRespuestas]);
-                      }}/>
-                    </Form.Group>
+                    </Row>
                     
-                    <Button variant="primary" type="submit">
-                      Submit
+                    
+                    {element.respuestas.map((e) =>(
+                      <Row className='filas2'>
+                        <Col sm md lg ={11} className='columnas'>
+                          <Form.Control id='inputResp' type="text" placeholder="Ingrese una respuesta" onChange={(e) =>{
+                            let cantidadRespuestas = e.target.value;
+                            setCantidadDeRespuestas([...cantidadDeRespuestas, cantidadRespuestas]);
+                          }}/>
+                        </Col>
+                        <Col>
+                          <AiFillCloseCircle id='iconoCerrar' onClick={() => eliminarRespuesta(e.idRespuesta, e, e.idPregunta)}/>
+                        </Col>
+                      </Row>
+                      
+                    ))}
+
+                    <Button variant="primary" onClick={() => agregarRespuesta(element.id)}>
+                      Agregar Respuesta
+                    </Button>
+
+                    <Button variant="primary" type="submit" onClick={agregarRespuesta}>
+                      x
                     </Button>
                 </Form>
                 
